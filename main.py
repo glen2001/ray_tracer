254bb1cf0b7af91a2e69512d9f0e44bf3ecce733
FakPls <gassmasterglen@gmail.com>
2023-04-15 00:02:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ SPHERE4.rayTracingMaterial.color = pg.math.Vector3(0, 255, 0)
 SPHERE5 = sphere(pg.math.Vector3(width/2 + 250, height/2 + 50, 300), 50)
 SPHERE5.rayTracingMaterial.color = pg.math.Vector3(255, 0, 255)
 
-SPHERE2 = sphere(pg.math.Vector3(width/2 + 200, -701, 200), 700)
+SPHERE2 = sphere(pg.math.Vector3(width/2 , -801,300), 800)
 SPHERE2.rayTracingMaterial.emissionStrength = 1
 SPHERE2.rayTracingMaterial.emissionColor = pg.math.Vector3(255, 255, 255)
 SPHERE2.rayTracingMaterial.color = pg.math.Vector3(0, 0, 0)
@@ -39,7 +39,7 @@ spheres.append(SPHERE3)
 spheres.append(SPHERE4)
 spheres.append(SPHERE5)
 
-iterations = 1
+iterations = 200
 iterPixels = []
 
 for k in range(iterations):
@@ -48,7 +48,7 @@ for k in range(iterations):
   for i in range(width):
     for j in range(height):
       RAY = ray(pg.math.Vector3(i, j, 0), pg.math.Vector3(0, 0, 1))
-      pixelColor.append(Trace(RAY, spheres, 2))
+      pixelColor.append(Trace(RAY, spheres, 4))
 
   TWOD_PixelColor = nest_list(pixelColor, width, height)
   iterPixels.append(TWOD_PixelColor)
