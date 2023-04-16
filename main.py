97e70bc71bf1b7fc45c2aa5065d6a13da98fa955
FakPls <gassmasterglen@gmail.com>
2023-04-16 03:02:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,25 +9,25 @@ t1 = time.time()
 print('Rendering Scene...')
 
 pg.init()
-fps = 60
+fps = 1
 fpsClock = pg.time.Clock()
  
-width, height = 800, 800
+width, height = 600, 600
 screen = pg.display.set_mode((width, height), pg.RESIZABLE)
 
 SPHERE1 = sphere(pg.math.Vector3(width/2 + 120, height/2, 400), 100)
 SPHERE1.rayTracingMaterial.color = pg.math.Vector3(99, 47, 196)
-SPHERE1.rayTracingMaterial.smoothness = 0.7
+SPHERE1.rayTracingMaterial.smoothness = 0.65
 
 SPHERE3 = sphere(pg.math.Vector3(width/2 - 120, height/2, 400), 100)
 SPHERE3.rayTracingMaterial.color = pg.math.Vector3(196, 72, 47)
-SPHERE3.rayTracingMaterial.smoothness = 0.7
+SPHERE3.rayTracingMaterial.smoothness = 0.65
 
 SPHERE4 = sphere(pg.math.Vector3(width/2, height/2 + 700, 600), 600)
 SPHERE4.rayTracingMaterial.color = pg.math.Vector3(189, 66, 111)
-SPHERE4.rayTracingMaterial.smoothness = .6
+SPHERE4.rayTracingMaterial.smoothness = .7
 
-SPHERE5 = sphere(pg.math.Vector3(width/3, height/4, 900), 300)
+SPHERE5 = sphere(pg.math.Vector3(width/3, height/4, 900), 200)
 SPHERE5.rayTracingMaterial.color = pg.math.Vector3(47, 189, 196)
 SPHERE5.rayTracingMaterial.smoothness = .7
 
@@ -36,7 +36,7 @@ SPHERE2.rayTracingMaterial.emissionStrength = 255
 SPHERE2.rayTracingMaterial.emissionColor = pg.math.Vector3(1, 1, 1)
 SPHERE2.rayTracingMaterial.color = pg.math.Vector3(0, 0, 0)
 
-SPHERE6 = sphere(pg.math.Vector3(width/2 + 400, height/2, 700), 100)
+SPHERE6 = sphere(pg.math.Vector3(width/2, height/2 + 100, 450), 50)
 SPHERE6.rayTracingMaterial.emissionStrength = 255
 SPHERE6.rayTracingMaterial.emissionColor = pg.math.Vector3(1, 1, 1)
 SPHERE6.rayTracingMaterial.color = pg.math.Vector3(0, 0, 0)
@@ -58,7 +58,7 @@ for k in range(iterations):
   for i in range(width):
     for j in range(height):
       RAY = ray(pg.math.Vector3(i, j, 0), pg.math.Vector3(0, 0, 1))
-      pixelColor.append(Trace(RAY, spheres, 4))
+      pixelColor.append(Trace(RAY, spheres, 5))
 
   TWOD_PixelColor = nest_list(pixelColor, width, height)
   iterPixels.append(TWOD_PixelColor)
